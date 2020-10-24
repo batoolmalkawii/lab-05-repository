9f9eec456d1acc9f2038ed348c9374dc31dfa8bd
Batool Malkawi <batoolmalkawii@gmail.com>
2020-10-24 16:00:16 +0300

    fixed server setup

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const { response } = require('express');
 const express = require('express');
-const { request } = require('http');
-const app = express();
 const PORT = process.env.PORT || 3000,
+const app = express();
 
 
 app.use(express.static('./public'));
+
 app.get('/', (request, response)=>{
     response.status(200).send('Hello');
 });
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT, ()=>{
     console.log(`Listening to port: ${PORT}`);
