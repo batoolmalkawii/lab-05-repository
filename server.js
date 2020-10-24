38eb58f9767d7505cca13e48f7adb7f14545bee7
Batool Malkawi <batoolmalkawii@gmail.com>
2020-10-24 15:48:59 +0300

    server init setup

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { response } = require('express');
+const express = require('express');
+const { request } = require('http');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./public'));
+app.get('/', (request, response)=>{
+    response.status(200).send('Hello');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to port: ${PORT}`);
+});
